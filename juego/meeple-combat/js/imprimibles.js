accda4002e10afd6effae57510af12fe689c7549
Fernando Castro <devfercastro@gmail.com>
2024-02-27 15:04:09 -0300

    First comment

---
+++ juego/meeple-combat/js/imprimibles.js
@@ -0,0 +1,36 @@
+packBtn.addEventListener('click', function () {
+
+
+    window.location = "../../juego/meeple-combat/recursos/2d/meeple-combat.pdf"
+
+})
+
+losetasBtn.addEventListener('click', function () {
+
+
+    window.location = "../../juego/meeple-combat/recursos/2d/losetas.pdf"
+
+})
+
+personajesBtn.addEventListener('click', function () {
+
+
+    window.location = "../../juego/meeple-combat/recursos/2d/personajes-marcas.pdf"
+
+})
+
+
+manualBtn.addEventListener('click', function () {
+
+
+    window.location = "../../juego/meeple-combat/recursos/manuales/manual-meeple-combat.pdf"
+
+})
+
+
+pack3dBtn.addEventListener('click', function () {
+
+
+    window.location = "../../juego/meeple-combat/recursos/3d/3mf-meeple-combat.rar"
+
+})
