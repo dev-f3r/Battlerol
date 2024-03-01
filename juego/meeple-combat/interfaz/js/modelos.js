10a73d0cdf8a03cd7cef3b22f2a2d0396ffa203f
Fernando Castro <devfercastro@gmail.com>
2024-03-01 19:17:55 -0300

    Modelo para los modales

--- juego/meeple-combat/interfaz/js/modelos.js
+++ juego/meeple-combat/interfaz/js/modelos.js
@@ -20,8 +20,8 @@ class BotonModal {
     armar() {
         const clases = this.clases.join(" ")
         const estilos = this.estilos.join(";")
-        const id = `${this.nombre}BTN`
-        const icono = `img/${this.nombre}`
+        const id = `${this.nombre}_btn`
+        const icono = `img/${this.nombre}ico.png`
 
         this.html =
             `<button class="${clases}" id="${id}" style="${estilos}">
@@ -31,5 +31,50 @@ class BotonModal {
 }
 
 class Modal {
-    
+    constructor(
+        nombre = "",
+        clases = [],
+        estilos = [],
+        botones = [],
+    ) {
+        this.nombre = nombre
+        this.clases = clases
+        this.estilos = estilos
+        this.botones = botones
+
+        this.html = ""
+
+        this.armar()
+    }
+
+    armar() {
+        const clases = this.clases.join(" ")
+        const estilos = this.estilos.join(";")
+        const id = `modal_${this.nombre}`
+
+        let contenido = ""
+
+        // ? Titulo
+        contenido += `<div class="item-modal"><span class="texto">${this.nombre.toUpperCase()}</span></div>\n`
+        // ? Boton cerrar modal
+        contenido += `<button class="item-modal" id="cerrar_modal_${this.nombre}"><img src="img/cerrar.png"></button>\n`
+
+        // ? Lista de botones
+        for (let i = 0; i < this.botones.length; i++) {
+            const boton = this.botones[i]
+            if (i === this.botones.length - 1) {
+                contenido += `<button class="item-modal"><img src="img/atras.png"></button>\n`
+                contenido += `${boton.html}\n`
+                contenido += `<button class="item-modal"><img src="img/adelante.png"></button>\n`
+            } else {
+                contenido += `${boton.html}\n`
+            }
+        }
+
+        this.html =
+            `<div class="${clases}" id="${id}" style="${estilos}">
+                ${contenido}
+            </div>`
+
+    }
 }
