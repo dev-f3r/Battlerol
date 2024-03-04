9e7e69b1c9701d0c8e023302783b0257170f6047
Fernando Castro <devfercastro@gmail.com>
2024-03-04 19:38:36 -0300

    Documentacion de modelos.js UI.js

--- juego/meeple-combat/interfaz/js/UI.js
+++ juego/meeple-combat/interfaz/js/UI.js
@@ -1,23 +1,34 @@
+// * ESTE ARCHIVO SE USA PARA GENERAR ELEMENTOS UI
+
+// Maximo de botones generales (se excluyen los botones de navegacion) por modal
 const maximo_botones = 13
 
+// Botones para cada modal
 const botones = {
+    // ? Lista de botones para modal personajes
     "personajes": nombrePersonajes.map((nombre, i) => {
-        const personaje = personajes[nombre]
+        // El nombre se extrae de la portada 'img/<nombre>.png' del personaje
         const filNombre = personaje.portada.match(/\/(.+?)\.png/)
-
+        // Si excede el maximo el boton se genera con "display: none;"
         const mostrar = i <= maximo_botones - 1 ? true : false
 
         return new BotonModal(filNombre[1], ["item-modal"], mostrar)
     }),
+    // ? Lista de botones para modal armas
     "armas": [],
+    // ? Lista de botones para modal equipo
     "equipo": [],
 }
 
+// Lista de modales
 const modales = Object.keys(botones).map(nombre => {
+    // Se trae la lista de botones y se pasa a la clase
     const contenido = botones[nombre]
+
     return new Modal(nombre, ["modal"], ["display:none"], contenido, maximo_botones)
 })
 
+// Elemento contenedor de los modales
 const main = document.getElementById("main")
 // * Insertar los modales al final de #main
 main.insertAdjacentHTML("beforeend", modales[0].html)
--- juego/meeple-combat/interfaz/js/modelos.js
+++ juego/meeple-combat/interfaz/js/modelos.js
@@ -1,4 +1,16 @@
+// * ESTE ARCHIVO CONTIENE LAS CLASES PARA GENERAR EL HTML DE ALGUNOS ELEMENTOS DEL UI
+
+/**
+ * ? Clase que representa un botón HTML.
+ */
 class Boton {
+    /**
+     * ? Constructor de la clase Boton.
+     * @param {string} nombre - El nombre del botón (id: nombre_btn).
+     * @param {string[]} clases - Las clases CSS del botón.
+     * @param {boolean} mostrar - Indica si el botón debe mostrarse o no (true: display: flex, false: display: none).
+     * @param {string} contenido - El contenido dentro del botón.
+     */
     constructor(
         nombre = "nada",
         clases = [],
@@ -12,9 +24,13 @@ class Boton {
 
         this.html = ""
 
+        // Se llama al método armar() para generar el HTML del botón.
         this.armar()
     }
 
+    /**
+     * ? Arma el HTML del boton
+     */
     armar() {
         const clases = this.clases.join(" ")
         const mostrar = this.mostrar ? "display: flex;" : "display: none;"
@@ -24,21 +40,45 @@ class Boton {
     }
 }
 
+/**
+ * ? Clase que representa un botón HTML.
+ */
 class BotonModal extends Boton {
+    /**
+     * ? Constructor de la clase Boton (id: "nombre"_btn, imgsrc: img/"nombre"ico.png).
+     * @param {string} nombre - El nombre del botón.
+     * @param {string[]} clases - Las clases CSS del botón.
+     * @param {boolean} mostrar - Indica si el botón debe mostrarse o no (true: display: flex, false: display: none).
+     * @param {string} contenido - El contenido dentro del botón.
+     */
     constructor(
         nombre = "nada",
         clases = [],
         mostrar = false,
     ) {
+        // Se genera la ruta del icono del botón basado en su nombre.
         const icono = `img/${nombre}ico.png`
 
+        // Llamada al constructor de la clase padre (Boton) con los parámetros proporcionados, incluyendo el icono.
         super(nombre, clases, mostrar, `<img src="${icono}" alt="${nombre}" />`)
 
+        // Se llama al método armar() para generar el HTML del botón.
         this.armar()
     }
 }
 
+/**
+ * ? Clase que representa un modal (menu de opciones emergente).
+ */
 class Modal {
+    /**
+     * ? Constructor de la clase Modal (id: modal_"nombre", ).
+     * @param {string} nombre - El nombre del modal.
+     * @param {string[]} clases - Las clases CSS del modal.
+     * @param {string[]} estilos - Los estilos CSS adicionales del modal.
+     * @param {Boton[]} botones - Los botones del modal.
+     * @param {number} maximo_botones - El número máximo de botones permitidos en el modal.
+     */
     constructor(
         nombre = "",
         clases = [],
@@ -54,9 +94,13 @@ class Modal {
 
         this.html = ""
 
+        // Se llama al método armar() para generar el HTML del modal.
         this.armar()
     }
 
+    /**
+     * ? Arma el HTML del modal
+     */
     armar() {
         const clases = this.clases.join(" ")
         const estilos = this.estilos.join(";")
@@ -64,16 +108,19 @@ class Modal {
 
         let contenido = ""
 
-        // ? Titulo
+        // Se agrega el título del modal
         contenido += `<div class="item-modal"><span class="texto">${this.nombre.toUpperCase()}</span></div>`
-        // ? Boton cerrar modal
+
+        // Se crea el botón para cerrar el modal
         const boton_cerrar = new Boton(`cerrar_modal_${this.nombre}`, ["item-modal"], true, `<img src="img/cerrar.png">`)
         contenido += boton_cerrar.html
 
-        // ? Lista de botones
+        // Se agregan los botones al contenido del modal
         for (let i = 0; i < this.botones.length; i++) {
             const boton = this.botones[i]
             if (i === this.maximo_botones - 1) {
+                // Si se alcanza el máximo de botones permitidos, se agregan botones de navegación
+
                 const boton_atras = new Boton(`atras_modal_${this.nombre}`, ["item-modal"], true, `<img src="img/atras.png">`)
                 const boton_adelante = new Boton(`adelante_modal_${this.nombre}`, ["item-modal"], true, `<img src="img/adelante.png">`)
 
@@ -85,6 +132,7 @@ class Modal {
             }
         }
 
+        // Se genera el HTML completo del modal
         this.html =
             `<div class="${clases}" id="${id}" style="${estilos}">
                 ${contenido}
