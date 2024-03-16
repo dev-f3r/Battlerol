7b1182a51ca9b109cd93a53e42cffe7e27db05fa
Fernando Castro <devfercastro@gmail.com>
2024-03-16 17:27:52 -0300

    Modo edicion y juego, mas despliege de modales

---
+++ juego/meeple-combat/interfaz/js/UIUtils.js
@@ -0,0 +1,23 @@
+let modo = "jugar"
+
+const boton_editar = document.getElementById("editarBtn")
+boton_editar.addEventListener("click", () => {
+    if(modo === "jugar") modo = "edicion"
+    else modo = "jugar"
+})
+
+const botones_modales_personajes = ["portadaBtn"]
+botones_modales_personajes.forEach(id => {
+    const el = document.getElementById(id)
+    el.addEventListener("click", () => {
+        if(modo === "edicion") modales.avatares.MostrarOcultarElemento()
+    })
+})
+
+const botones_modales_armas = ["arma1ImgBtn", "arma1TxtBtn", "arma2ImgBtn", "arma2TxtBtn"]
+botones_modales_armas.forEach(id => {
+    const el = document.getElementById(id)
+    el.addEventListener("click", () => {
+        if(modo === "edicion") modales.armas_comunes.MostrarOcultarElemento()
+    })
+})
