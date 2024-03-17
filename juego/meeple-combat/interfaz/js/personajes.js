1310bbbbaadafc03f4b8bee4e70c00bdf40d95c3
Fernando Castro <devfercastro@gmail.com>
2024-03-17 00:44:37 -0300

    avatares de prueba eliminados

--- juego/meeple-combat/interfaz/js/personajes.js
+++ juego/meeple-combat/interfaz/js/personajes.js
@@ -797,58 +797,6 @@ const avatares = {
     },
 
     // ! NUEVOS AVATARES HUMANOS DEBEN IR DESPUES DE "nuevopj"
-    "test1": {
-        nombre: "test1",
-        portada: "img/nada.png",
-        icono: "",
-        descripcion: "Sin descripción.",
-
-        ataque: 0,
-        esquiva: 0,
-        bloqueo: 0,
-        velocidad: 0,
-        vida: 0,
-        vidaMaxima: 0,
-        poder: 0,
-        poderMaximo: 0,
-
-        arma1: "nada",
-        arma2: "nada",
-
-        equipo1: "nada",
-        equipo2: "nada",
-        equipo3: "nada",
-
-        habilidad1: "habilidad 1",
-        habilidad2: "habilidad 2",
-        habilidad3: "habilidad 3",
-    },
-    "test2": {
-        nombre: "test2",
-        portada: "img/nada.png",
-        icono: "",
-        descripcion: "Sin descripción.",
-
-        ataque: 0,
-        esquiva: 0,
-        bloqueo: 0,
-        velocidad: 0,
-        vida: 0,
-        vidaMaxima: 0,
-        poder: 0,
-        poderMaximo: 0,
-
-        arma1: "nada",
-        arma2: "nada",
-
-        equipo1: "nada",
-        equipo2: "nada",
-        equipo3: "nada",
-
-        habilidad1: "habilidad 1",
-        habilidad2: "habilidad 2",
-        habilidad3: "habilidad 3",
-    },
 }
 
 const avataresOcultos = {
