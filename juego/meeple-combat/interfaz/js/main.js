391507953169b0a8353791bb217e7e026fdaa887
Fernando Castro <devfercastro@gmail.com>
2024-03-21 17:22:53 -0300

    Lista de personajes jugables

--- juego/meeple-combat/interfaz/js/main.js
+++ juego/meeple-combat/interfaz/js/main.js
@@ -5,12 +5,33 @@
  * @const {Personaje[]}
  */
 const personajes = [
-    new Personaje("meeple"),
-    new Personaje("esbirro 1"),
-    new Personaje("esbirro 2"),
-    new Personaje("esbirro 3"),
-    new Personaje("esbirro 4"),
-    new Personaje("esbirro 5"),
+    new Personaje({
+        nombre: "meeple",
+    }),
+    new Personaje({
+        nombre: "esbirro 1",
+    }),
+    new Personaje({
+        nombre: "esbirro 2",
+    }),
+    new Personaje({
+        nombre: "esbirro 3",
+    }),
+    new Personaje({
+        nombre: "esbirro 4",
+    }),
+    new Personaje({
+        nombre: "esbirro 5",
+    }),
 ]
 
+/**
+ * ? Indica el indice del personaje seleccionado
+ * @var {number}
+ */
+let personaje_seleccionado = 0
+/**
+ * ? Indica el modo de juego
+ * @var {string}
+ */
 let modo = "jugar"
