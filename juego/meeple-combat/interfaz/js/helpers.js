b71d9b871e59ac520c0b073163da762f72d6a49e
Fernando Castro <devfercastro@gmail.com>
2024-03-25 23:03:02 -0300

    Archivo con funciones helpers

---
+++ juego/meeple-combat/interfaz/js/helpers.js
@@ -0,0 +1,37 @@
+/**
+ * ? Quita cualquier tipo de acentuación.
+ * @param {string} text - El texto a modificar.
+ * @returns {string} El texto sin acentos.
+ */
+function quitarAcentos(text) {
+    return text
+        .normalize('NFD') // Descopone caracteres acentuados en su caracter base y su acento.
+        .replace(/[\u0300-\u036f]/g, '') // Elimina los acentos.
+}
+
+/**
+ * ? Capitaliza la primera letra de un string.
+ *
+ * @param {string} texto - El string que se va a capitalizar.
+ * @returns {string} - El string con la primera letra en mayúscula.
+ */
+function capitalizarPrimeraLetra(texto) {
+    // Verifica si el texto está vacío o es nulo y devuelve el mismo texto sin cambios
+    if (!texto) {
+        return texto;
+    }
+
+    // Capitaliza la primera letra del texto y la concatena con el resto del texto en minúsculas
+    return texto.charAt(0).toUpperCase() + texto.slice(1).toLowerCase()
+}
+
+/**
+ * ? Elimina los espacios en una cadena de texto.
+ * @param {string} texto - El texto con espacios.
+ * @returns {string} El texto si espacios.
+ */
+function quitarEspacios(texto) {
+    return texto
+        .split(" ") // Crea un arreglo usando como separador los espacios.
+        .join("") // Une el arreglo en un string.
+}
